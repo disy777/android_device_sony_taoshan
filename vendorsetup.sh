cfdffd48c8548f8ee60b2ee2f3a30d95caf1a4da
Sohamlad7 <sohamlad7@gmail.com>
2017-04-19 20:20:08 +0530

    taoshan: build: Add AOSP 8.0 build support
    Change-Id: Ibfb1c55026190f680e568eee7a38029b9defb846
    Signed-off-by: Sohamlad7 <sohamlad7@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,5 +14,5 @@
 # limitations under the License.
 #
 
-add_lunch_combo lineage_taoshan-eng
-add_lunch_combo lineage_taoshan-userdebug
+add_lunch_combo aosp_taoshan-eng
+add_lunch_combo aosp_taoshan-userdebug
